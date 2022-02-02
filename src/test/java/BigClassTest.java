b209fd77b402e008d59252477d91b727fe30cc8b
Linus Callheim <stoltefar@gmail.com>
2022-02-02 15:07:08 +0100

    Added tests for getters, setters and stringToUpperCase-method.

--- src/test/java/BigClassTest.java
+++ src/test/java/BigClassTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -38,6 +40,54 @@ class BigClassTest {
         //Assert
         assertInstanceOf(BigClass.class, testObject);
     }
+
+    @Test
+    public void checkSetNumberWithNegative() {
+        //Arrange
+        testObject = new BigClass();
+
+        testObject.setNumber(-1);
+        int expected = 0;
+        int actual = testObject.getNumber();
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 42, 765328})
+    public void checkSetNumber(int input) {
+        testObject = new BigClass();
+
+        testObject.setNumber(input);
+        int expected = input;
+        int actual = testObject.getNumber();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkSetAndGetText() {
+        testObject = new BigClass();
+
+        testObject.setText("Hallå Hej!");
+        String expected = "Hallå Hej!";
+        String actual = testObject.getText();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkStringToUppercaseMethod() {
+        testObject = new BigClass();
+
+        testObject.setText("Hallå Hej!");
+        testObject.stringToUpperCase();
+        String expected = "HALLÅ HEJ!";
+        String actual = testObject.getText();
+
+        assertEquals(expected, actual);
+    }
+
 
 
 }
