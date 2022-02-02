b5f3ef1af9af1444cf6ea2dd43ab71513a891de8
Joakim <joakim.sorkka@iths.se>
2022-02-02 13:17:21 +0100

    First version

---
+++ src/main/java/BigClass.java
@@ -0,0 +1,19 @@
+public class BigClass {
+    private int number = 0;
+    private String text = null;
+
+    public BigClass() {
+
+    }
+    public BigClass(int number) {
+        this.number = number;
+    }
+    public BigClass(String text) {
+        this.text = text;
+    }
+    public BigClass(int number,String text) {
+        this.number = number;
+        this.text = text;
+    }
+
+}
